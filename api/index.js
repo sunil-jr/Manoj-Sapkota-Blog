69c52eaa2f2efea91b95069eccd6523d0bae6134
sunilsubedi <subedisunil495@gmail.com>
2024-08-26 01:23:40 +0545

    Created signup functionality

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { json } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
@@ -9,9 +10,11 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err => { console.log(err)}))
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
 })
 
 app.use('/api/user', userRoutes );
+app.use('/api/auth', authRoutes);
