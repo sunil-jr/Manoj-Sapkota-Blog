94c922385225b13fa2b2d7c8253ad6a398a44bd9
sunilsubedi <subedisunil495@gmail.com>
2024-08-26 01:34:35 +0545

    Created error handler

--- api/index.js
+++ api/index.js
@@ -18,3 +18,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes );
 app.use('/api/auth', authRoutes);
+
+// Middleware for error
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode, 
+        message 
+    });
+});
