b40771503d97f93b2266067945009d0aff8c8e60
sunilsubedi <subedisunil495@gmail.com>
2024-08-25 22:13:03 +0545

    Created User Module

--- api/index.js
+++ api/index.js
@@ -3,16 +3,20 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-mongoose.connect(process.env.MONGO)
-
-
-.then(
-    () => { console.log('MongoDB is connected!') }
-).catch((err => { console.log(err)}))
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDB is connected!') 
+}).catch((err => { console.log(err)}))
 
 const app = express();
 
+const muji = (req,res) => {
+    res.end( JSON.stringify({
+        "message": "Muji!!!"
+    }));
+}
+
+app.get("/", muji)
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
-}
-)
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
